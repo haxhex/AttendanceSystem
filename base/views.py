dc7440783846b828c1142af022ca7342ffb92a23
Helia Ghorbani <heliaghorbani2001@gmail.com>
2022-11-12 04:27:12 +0330

    add some templates

--- base/views.py
+++ base/views.py
@@ -1,4 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User 
+from django.contrib import messages 
 
 def home(request):
         return render(request, 'base/home.html')
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exist')
+            
+    return render(request, 'base/login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
