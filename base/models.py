93bb235c6d041e1d613aac24a495b8696d5d413c
Mahdie Rahmati <rahmatimahdie9@gmail.com>
2022-11-13 14:22:41 +0330

    added employee model and in-out model fixed

--- base/models.py
+++ base/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+
 class In_Out(models.Model):
     in_time = models.TimeField()
     out_time = models.TimeField()
     employee = models.ForeignKey(
-        "employee.User",
+        "Employee",
         on_delete=models.CASCADE,
     )
