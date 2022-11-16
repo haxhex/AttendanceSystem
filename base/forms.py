ca8aaed35af9f0b4c0098bf4c6e9384cabd0b981
Mahdie Rahmati <rahmatimahdie9@gmail.com>
2022-11-16 18:13:57 +0330

    added change password form

--- base/forms.py
+++ base/forms.py
@@ -1,9 +1,17 @@
 from django.forms import ModelForm
 from .models import Employee
+from django.contrib.auth.forms import SetPasswordForm
+from django.contrib.auth import get_user_model
 
 class EmployeeForm(ModelForm):
     class Meta:
         model = Employee
         fields = ['first_name', 'last_name', 'email', 'phone_number']
+
+class SetPasswordForm(SetPasswordForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['new_password1', 'new_password2']
+
 
         
