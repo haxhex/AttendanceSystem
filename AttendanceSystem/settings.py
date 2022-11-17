550398711b31e2e40fe0836da77ebb4823090ca5
Mahdie Rahmati <rahmatimahdie9@gmail.com>
2022-11-17 15:32:40 +0330

    changed setting and url of project

--- AttendanceSystem/settings.py
+++ AttendanceSystem/settings.py
@@ -102,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
