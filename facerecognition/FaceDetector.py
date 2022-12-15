db3354ce34c5f88c1e3afdbe1e572577d2a0f969
Abolfazl Ardeshir <abolfazlat2000@outlook.com>
2022-12-16 01:57:08 +0330

    change code designe

--- facerecognition/FaceDetector.py
+++ facerecognition/FaceDetector.py
@@ -26,40 +26,18 @@ class FaceDetector(object):
             pass
 
         return frame
+    
     def detect(self,frame):
         boxes, probs, landmarks = self.mtcnn.detect(frame, landmarks=True)
         return boxes, probs, landmarks
 
 
-    def run(self):
+    def run(self,frame):
         """
             Run the FaceDetector and draw landmarks and boxes around detected faces
         """
-        cap = cv2.VideoCapture(0)
-
-        while True:
-            ret, frame = cap.read()
-            try:
-                # detect face box, probability and landmarks
-                
-                # draw on frame
-                image = Image.fromarray(frame)
-                boxes, probs, landmarks = self.detect(image)
-                
-                frame_draw = frame.copy()
-                frame_draw = Image.fromarray(frame_draw)
-
-                self._draw(frame_draw, boxes, probs, landmarks)
-
-            except:
-                pass
-
-            # Show the frame
-            cv2.imshow('Face Detection', np.asarray(frame_draw))
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        cap.release()
-        cv2.destroyAllWindows()
+        # draw on frame
+        boxes, probs, landmarks = self.detect(frame)
+        
+        self._draw(frame, boxes, probs, landmarks)
         
