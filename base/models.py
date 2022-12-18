93f4a972303b12fce5137a3742ca362e50b63d62
Mahdie Rahmati <rahmatimahdie9@gmail.com>
2022-12-18 15:14:35 +0330

    fixed differences

--- base/models.py
+++ base/models.py
@@ -9,17 +9,17 @@ def mentor_photos(instance, filename):
 
     
 class Employee(models.Model):
-	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-	first_name = models.CharField(max_length=200, null=True)
-	last_name = models.CharField(max_length=200, null=True)
-	mobile_number = models.CharField(max_length=200, null=True, blank=True)
-	email = models.EmailField(max_length=200, null=True, unique=True)
-	profile_picture = models.ImageField(default="default_profile.png", null=True, blank=True, upload_to=mentor_photos)	
-	department = models.CharField(max_length=200, null=True)	
-	position = models.CharField(max_length=200, null=True)
-
-	def __str__(self):
-		return str(self.user)
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
+    mobile_number = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, unique=True)
+    profile_picture = models.ImageField(default="default_profile.png", null=True, blank=True, upload_to=mentor_photos)	
+    department = models.CharField(max_length=200, null=True)	
+    position = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return str(self.user)
 
 
 class In_out(models.Model):
